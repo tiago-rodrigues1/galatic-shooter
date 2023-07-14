7d9d3f865e9c5de21edc82c80798df93a1d2380c
tiago-rodrigues1 <tiagords127@gmail.com>
2023-07-14 17:47:46 -0300

    add nome do player na tela

--- js/salvar.js
+++ js/salvar.js
@@ -1,11 +1,33 @@
-let campo = $("#nome");
 
-  $('#nome[type=text]').on('input', function() {
-    if ($(this).val() !== '0') {
-        $('#play').removeAttr("disabled");
-    }
-    else {
-        $('#play').attr('disabled', 'disabled');
-    }
+
+$(document).ready(function() {
+    let nomeJogador = "";
+    let campo = $("#nome");
+    let comecarBtn = $('#play');
+
+    $(campo).on('input', function() {
+        if ($(this).val() !== '0') {
+            $(comecarBtn).removeAttr("disabled");
+        }
+        else {
+            $(comecarBtn).attr('disabled');
+        }
+
+        nomeJogador = $(this).val();
+    });
+
+    $(comecarBtn).click(function() {
+        window.location.href = `galatic.html?nome=${nomeJogador}`;
+    });
+    
+    $('#player').ready(function() {
+        let urlParams = window.location.search;
+
+        if (urlParams) {
+            let urlParamsJogador = urlParams.split('=')[1];
+            $('#player').html(urlParamsJogador);
+        }
+    });
+
+
 });
-
