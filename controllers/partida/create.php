1a83a960bde669262f0d594c0a079a258240c2a5
tiago-rodrigues1 <tiagords127@gmail.com>
2023-10-29 22:37:55 -0300

    create controller

---
+++ controllers/partida/create.php
@@ -0,0 +1,18 @@
+<?php
+require "../../models/Partida.php";
+
+$jogador = $_GET["txtJogador"];
+$acertos = $_GET["numAcertos"];
+$erros = $_GET["numErros"];
+
+
+$partida = new Partida($jogador, $acertos, $erros);
+$response = $partida->save();
+
+if ($response) {
+    header("Location:/gameOver.html");
+} else {
+    header("Location:/index.html");
+}
+
+?>
